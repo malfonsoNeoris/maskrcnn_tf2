f2b325d35bd25ca28eb111ae278c4a786293d694
malfonsoNeoris <manuel.alfonso@neoris.com>
2021-08-18 16:02:43 -0300

    xxx

--- src/plates_train.py
+++ src/plates_train.py
@@ -6,7 +6,7 @@ import tensorflow as tf
 from common.utils import tf_limit_gpu_memory
 from model import mask_rcnn_functional
 from preprocess import augmentation as aug
-from samples.coco import coco
+from samples.plates import plates
 from training import train_model
 
 if __name__ == '__main__':
@@ -19,15 +19,15 @@ if __name__ == '__main__':
     # Load Mask-RCNN config
     from common.config import CONFIG
 
-    CONFIG.update(coco.COCO_CONFIG)
-    CONFIG.update({'image_shape': (1024, 1024, 3),
+    CONFIG.update(plates.COCO_CONFIG)
+    CONFIG.update({'image_shape': (256, 256, 3),
                    'image_resize_mode': 'square',
-                   'img_size': 1024,
-                   'image_max_dim': 1024,
-                   'backbone': 'resnet50',
-                   'epochs': 150,
-                   'batch_size': 2,
-                   'images_per_gpu': 2,
+                   'img_size': 256,
+                   'image_max_dim': 256,
+                   'backbone': 'resnet18',
+                   'epochs': 50,
+                   'batch_size': 1,
+                   'images_per_gpu': 1,
                    'train_bn': True,
                    'use_multiprocessing': True,
                    'workers': mp.cpu_count()
@@ -36,13 +36,11 @@ if __name__ == '__main__':
                   )
 
     # Set folder for coco dataset
-    base_dir = r''
+    base_dir = r'/mnt/data/cx-ir/patentes_aug'
 
     # Initialize training and validation datasets
-    train_dataset = coco.CocoDataset(dataset_dir=base_dir,
+    train_dataset = plates.PlateDataset(dataset_dir=base_dir,
                                      subset='train',
-                                     year=2017,
-                                     auto_download=True,
                                      # SegmentationDataset necessary parent attributes
                                      augmentation=aug.get_training_augmentation(
                                          image_size=CONFIG['img_size'],
@@ -51,10 +49,8 @@ if __name__ == '__main__':
                                      **CONFIG
                                      )
 
-    val_dataset = coco.CocoDataset(dataset_dir=base_dir,
-                                   subset='val',
-                                   year=2017,
-                                   auto_download=True,
+    val_dataset = plates.PlateDataset(dataset_dir=base_dir,
+                                   subset='valid',
                                    # SegmentationDataset necessary parent attributes
                                    augmentation=aug.get_validation_augmentation(
                                        image_size=CONFIG['img_size'],
@@ -64,6 +60,8 @@ if __name__ == '__main__':
                                    )
 
     # Init Mask-RCNN model
+    CONFIG['callback']['save_dir'] = r'/home/user/MaskTests/maskrcnn_tf2/result_models/resnet18_256x256'
+
     model = mask_rcnn_functional(config=CONFIG)
 
     # Train
